54bd0cf798bc336ed6ec7add9be8a3e4ad73ab5f
pengqiang.li <pengqiang.li@accenture.com>
2024-07-15 17:45:54 +0800

    init

---
+++ src/main/java/org/solar/metaj/MetaJApplication.java
@@ -0,0 +1,13 @@
+package org.solar.metaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaJApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaJApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/solar/metaj/MetaJApplicationTests.java
@@ -0,0 +1,13 @@
+package org.solar.metaj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaJApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
